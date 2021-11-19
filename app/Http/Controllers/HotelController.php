794f9a4af1ebf4f9d8c369ce85e2799ea4859025
Enzo Carrión <enz10.cr@gmail.com>
2021-11-19 17:23:02 -0500

    v0.1

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -11,9 +11,9 @@ class HotelController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
+    public function index() {
+        $hotels = Hotel::all();
+        return view('hotels')->with('hotels', $hotels);
     }
 
     /**
